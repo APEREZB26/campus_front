d51d397469aa2410b2b06ba37b5a846d0022f15a
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-12-20 10:34:44 -0500

    [ADD] Prettier to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import {useEffect, useState} from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
-  const [students, setStudents] = useState([])
+  const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    fetch('https://campus-project.herokuapp.com/student')
-      .then(res => res.json())
-      .then(data => setStudents(data))
-  }, [])
-
+    fetch("https://campus-project.herokuapp.com/student")
+      .then((res) => res.json())
+      .then((data) => setStudents(data));
+  }, []);
 
   return (
     <div className="App">
@@ -19,18 +17,18 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <ul className="flex flex-wrap">
-          {
-            students.map((student) => (
-              <li>
-                <p>{student.name} - {student.lastnames}</p>
-                <p>{student.email}</p>
-              </li>
-            ))
-          }
+          {students.map((student) => (
+            <li>
+              <p>
+                {student.name} - {student.lastnames}
+              </p>
+              <p>{student.email}</p>
+            </li>
+          ))}
         </ul>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
